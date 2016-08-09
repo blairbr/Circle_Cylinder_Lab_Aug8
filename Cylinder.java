8b88f99d39e05c6f6ed73bc5fe2b6562f2b33246
Your Name <blair.j.brown@gmail.com>
2016-08-09 10:52:57 -0400

    first commit

---
+++ Cylinder.java
@@ -0,0 +1,58 @@
+
+public class Cylinder extends Circle {
+	// property
+	private double height;
+
+	// getter and setter
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	// CONSTRUCTORS
+
+	// constructor #1
+	public Cylinder(double radius, double height, String color) {
+		super(radius, color);
+		// this.height = height;
+		setHeight(height);
+	}
+
+	// constructor #2
+	public Cylinder(double radius, double height) {
+		super(radius);
+		setHeight(height);
+	}
+
+	// constructor #3
+	public Cylinder(double radius) {
+		super(radius);
+		height = 1.0;
+	}
+
+	// constructor #4
+	public Cylinder() {
+		super();
+		height = 1.0;
+	}
+
+	// Method get Volume
+	public double getVolume() {
+		double volume = getArea() * getHeight();
+		return volume;
+	}
+
+	// Method print volume
+	public void printVolume() {
+		System.out.println(getVolume());
+	}
+
+	@Override
+	public double getArea() {
+		return ((2 * Math.PI * getRadius() * getHeight()) + 2 * Math.PI * getRadius() * getRadius());
+
+	}
+
+}
