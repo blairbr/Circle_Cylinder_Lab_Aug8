a08331940b67a856bc67f1b7d423b1a271cd8ced
Your Name <blair.j.brown@gmail.com>
2016-08-09 10:53:33 -0400

    first commit

---
+++ LabAug8_MAIN.java
@@ -0,0 +1,13 @@
+
+public class LabAug8_MAIN {
+	
+	public static void main(String[] args) {
+		Circle cir1 = new Circle (3.0, "purple");
+		Cylinder cyl1 = new Cylinder(5.0, 5.0, "red" );
+				
+		cir1.printArea();
+		cyl1.printVolume();
+		cyl1.printArea();
+		
+	}
+}
